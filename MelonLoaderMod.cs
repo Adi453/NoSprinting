d640226bcc07250ece5005ab269c3297c72c2275
Adi <Adi453@users.noreply.github.com>
2022-07-16 18:49:58 +0300

    Add project files.

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using StressLevelZero.Rig;
+using UnityEngine;
+using ModThatIsNotMod.BoneMenu;
+using Colour = UnityEngine.Color;
+
+namespace NoSprinting
+{
+    public static class BuildInfo
+    {
+        public const string Name = "NoSprinting"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Adi"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class NoSprinting : MelonMod
+    {
+        ControllerRig rig;
+        public bool enabled = false;
+        public override void OnApplicationStart()
+        {
+            var cat = MenuManager.CreateCategory("Sprinting", Colour.white);
+            cat.CreateBoolElement("Enabled", Colour.white, enabled, delegate (bool value) { enabled = value; });
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) => rig = Object.FindObjectOfType<ControllerRig>();
+        public override void OnUpdate()
+        {
+            if (!enabled && rig != null && rig._jogToggle)
+                rig._jogToggle = false;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(NoSprinting.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(NoSprinting.BuildInfo.Company)]
+[assembly: AssemblyProduct(NoSprinting.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + NoSprinting.BuildInfo.Author)]
+[assembly: AssemblyTrademark(NoSprinting.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(NoSprinting.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(NoSprinting.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(NoSprinting.NoSprinting), NoSprinting.BuildInfo.Name, NoSprinting.BuildInfo.Version, NoSprinting.BuildInfo.Author, NoSprinting.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
